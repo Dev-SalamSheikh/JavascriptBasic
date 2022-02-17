3aa7c99e923f33812bee111e5c560a023648d75b
Salam Sheikh <dev.salamsheikh@gmail.com>
2022-02-18 05:21:18 +0600

    practiced on 17 feb 2022

---
+++ continue.js
@@ -0,0 +1,7 @@
+for (let i = 1; i <= 10; i++) {
+    if( i === 3 || i === 9){
+        continue;
+    }else{
+        console.log(i);
+    }
+}
---
+++ nestedloop.js
@@ -0,0 +1,9 @@
+let n = 10;
+
+for( var i = 1; i <= n; i++){
+    var result = '';
+    for( var j = 1; j <= n; j++){
+        result += '* '
+    }
+    console.log(result);
+} 
